be2f35c451da268bffe035e3a92490ca886a7932
aeriforme <arianna.formenti@gmail.com>
2022-03-25 00:19:02 +0100

    shuffled words

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ $(document).ready(function(){
       {
        var words_data = data.split(/\r?\n|\r/);
        var table_data = '<table class="table table-bordered table-striped">';
+       
+       var shuffled_words_data = shuffle(words_data);
+
        for(var count = 0; count<words_data.length; count++)
        {
-        var cell_data = words_data[count].split(",");
+
+
+        var cell_data = shuffled_words_data[count].split(",");
         table_data += '<tr>';
         for(var cell_count=0; cell_count<cell_data.length; cell_count++)
         {
@@ -31,3 +36,23 @@ $(document).ready(function(){
     });
     
    });
+
+
+   function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
