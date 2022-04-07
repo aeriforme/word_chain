55cf7c1deefa87d5e991f7b6aa13f1af9e15fb2b
aeriforme <arianna.formenti@gmail.com>
2022-04-07 10:27:13 +0200

    added clustering with aff prop

--- generator.py
+++ generator.py
@@ -90,7 +90,25 @@ while i < chain_length:
     if len(ant)>0: options.append(ant)    
 
 
-    # SEMANTICS -> AI 
+
+    from sklearn.cluster import AffinityPropagation
+    import distance
+    
+    
+    words = list(english_words) #So that indexing with a list will work
+    lev_similarity = -1*np.array([[distance.levenshtein(w1,w2) for w1 in words] for w2 in words])
+    
+    affprop = AffinityPropagation(affinity="precomputed", damping=0.5)
+    affprop.fit(lev_similarity)
+    for cluster_id in np.unique(affprop.labels_):
+        exemplar = words[affprop.cluster_centers_indices_[cluster_id]]
+        cluster = np.unique(words[np.nonzero(affprop.labels_==cluster_id)])
+        cluster_str = ", ".join(cluster)
+        print(" - *%s:* %s" % (exemplar, cluster_str))
+
+
+
+
 
     flat_options = [item for sublist in options for item in sublist]    
     num_options = len(flat_options)
